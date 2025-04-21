8d3ba249d2ef4682a6a9f0939dbf7f0486781611
blackburnwt <pullblue1@gmail.com>
2025-04-21 14:54:29 +0200

    init commit

---
+++ src/main/java/com/search/immo_queue/ImmoQueueJavaApplication.java
@@ -0,0 +1,13 @@
+package com.search.immo_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoQueueJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoQueueJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/search/immo_queue/ImmoQueueJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.immo_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoQueueJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
